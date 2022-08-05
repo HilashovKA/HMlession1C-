046599c207f26f35606cad8adf1d97ee3cc94e1d
lavr174 <lavr174@icloud.com>
2022-08-05 13:55:21 +0300

    task8

---
+++ Task8/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает число (N)
+//а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число: ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+int res = 1;
+       
+if (res == 1)
+{
+    res += 1;
+}
+
+while (res <= num)
+{
+    Console.Write(res + " ");
+    res += 2;
+}
+
+
+
